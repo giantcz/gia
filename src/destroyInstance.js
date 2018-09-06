7faeff195d620044230e56469e8ce651674966d3
Georgy Marchuk <gmarcuk@gmail.com>
2018-09-06 22:19:05 +0200

    rename destroy method to unmount

--- src/destroyInstance.js
+++ src/destroyInstance.js
@@ -10,7 +10,7 @@ export default function destroyInstance(element){
     const instance = getComponentFromElement(element);
     if (instance) {
         const name = instance._name;
-        instance.destroy();
+        instance.unmount();
         element['__gia_component__'] = null;
         if (config.get('log')) {
             console.info(`Removed component "${name}".`);
